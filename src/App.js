b35434918cacfe40b3564c4c080a570a2dfbfe1f
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-04-08 18:08:56 +0300

    add searchbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import FetchImage from './services/fetchImage';
 import Loaders from './components/Loader';
 import { ToastContainer } from 'react-toastify';
+import Searchbar from './components/Searchbar';
 
 import 'react-toastify/dist/ReactToastify.css';
 /* import Modal from './components/Modal'; */
@@ -16,10 +17,11 @@ class App extends Component {
   handleFormSubmit = searchImage => {
     this.setState({ searchImage });
   };
+
   render() {
     return (
       <div className="App">
-        Hello
+        <Searchbar onSubmit={this.handleFormSubmit} />
         <Loaders />
         <ToastContainer autoClose={3000} position="top-right" type="default" />
       </div>
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,49 @@
+import { Component } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import s from './Searchbar.module.scss';
+
+import { BsSearch } from 'react-icons/bs';
+
+/* import findImage from '../Searchbar/search.png'; */
+
+export default class Searchbar extends Component {
+  state = {
+    imageSearch: '',
+  };
+
+  handleNameChange = e => {
+    this.setState({ imageSearch: e.currentTarget.value.toLowerCase() });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.imageSearch.trim() === '') {
+      toast.error('Pleese enter correct value!');
+      return;
+    }
+    this.props.onSubmit(this.state.imageSearch);
+    this.setState({ imageSearch: '' });
+  };
+  render() {
+    return (
+      <div>
+        <header className={s.searchbar}>
+          <form className={s.searchForm} onSubmit={this.handleSubmit}>
+            <button type="submit" className={s.searchFormButton}>
+              <BsSearch style={{ width: 20, height: 20 }} />
+              {/* <span className={s.searchFormButtonLabel}>Search</span> */}
+            </button>
+            <input
+              className={s.searchFormInput}
+              type="text"
+              autoComplete="off"
+              autoFocus
+              placeholder="Enter a query to search for the image"
+              onChange={this.handleNameChange}
+            />
+          </form>
+        </header>
+      </div>
+    );
+  }
+}
