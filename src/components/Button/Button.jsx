561f4c715e4ba61fefe232035d54715e0f700ffb
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-04-08 15:10:41 +0300

    add button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import s from './Button.module.css';
+
+export default function Button({ onClick }) {
+  return (
+    <div className={s.buttonLoadContainer}>
+      <button
+        type="button"
+        className={s.buttonLoad}
+        onClick={onClick}
+        aria-label="Load more"
+      >
+        Load more
+      </button>
+    </div>
+  );
+}
+
+Button.propTypes = {
+  incrementPage: PropTypes.func,
+};
