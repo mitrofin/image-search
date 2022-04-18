a053457b8bdddc7acf860a256d6ada2a39adff25
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-04-18 21:30:11 +0300

    adapt searchbar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
 import Searchbar from './components/Searchbar';
 import ImageGallery from './components/ImageGallery';
+import { useState } from 'react';
 
 import 'react-toastify/dist/ReactToastify.css';
 /* import Modal from './components/Modal'; */
 
 /* console.log(FetchImage('car')); */
 
-class App extends Component {
-  state = {
-    searchImage: '',
+function App() {
+  const [searchImage, setSearchImage] = useState('');
+
+  const handleFormSubmit = searchImage => {
+    setSearchImage(searchImage);
   };
 
-  handleFormSubmit = searchImage => {
-    this.setState({ searchImage });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery searchImage={this.state.searchImage} />
-        <ToastContainer autoClose={3000} position="top-right" type="default" />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ImageGallery searchImage={searchImage} />
+      <ToastContainer autoClose={3000} position="top-right" type="default" />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import s from './Searchbar.module.scss';
@@ -7,44 +7,41 @@ import { BsSearch } from 'react-icons/bs';
 
 /* import findImage from '../Searchbar/search.png'; */
 
-export default class Searchbar extends Component {
-  state = {
-    imageSearch: '',
-  };
+export default function Searchbar({ onSubmit }) {
+  const [imageSearch, setImageSearch] = useState('');
 
-  handleNameChange = e => {
-    this.setState({ imageSearch: e.currentTarget.value.toLowerCase() });
+  const handleNameChange = e => {
+    setImageSearch(e.currentTarget.value.toLowerCase());
   };
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.state.imageSearch.trim() === '') {
+    if (imageSearch.trim() === '') {
       toast.error('Pleese enter correct value!');
       return;
     }
-    this.props.onSubmit(this.state.imageSearch);
-    this.setState({ imageSearch: '' });
+    onSubmit(imageSearch);
+    setImageSearch('');
   };
-  render() {
-    return (
-      <div>
-        <header className={s.searchbar}>
-          <form className={s.searchForm} onSubmit={this.handleSubmit}>
-            <button type="submit" className={s.searchFormButton}>
-              <BsSearch style={{ width: 20, height: 20 }} />
-              {/* <span className={s.searchFormButtonLabel}>Search</span> */}
-            </button>
-            <input
-              className={s.searchFormInput}
-              type="text"
-              value={this.state.imageSearch}
-              autoComplete="off"
-              autoFocus
-              placeholder="Enter a query to search for the image"
-              onChange={this.handleNameChange}
-            />
-          </form>
-        </header>
-      </div>
-    );
-  }
+
+  return (
+    <div>
+      <header className={s.searchbar}>
+        <form className={s.searchForm} onSubmit={handleSubmit}>
+          <button type="submit" className={s.searchFormButton}>
+            <BsSearch style={{ width: 20, height: 20 }} />
+            {/* <span className={s.searchFormButtonLabel}>Search</span> */}
+          </button>
+          <input
+            className={s.searchFormInput}
+            type="text"
+            value={imageSearch}
+            autoComplete="off"
+            autoFocus
+            placeholder="Enter a query to search for the image"
+            onChange={handleNameChange}
+          />
+        </form>
+      </header>
+    </div>
+  );
 }
